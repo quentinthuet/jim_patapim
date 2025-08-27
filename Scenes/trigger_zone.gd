812413e5cd34875b14b04639744f10b8568966ec
quentinthuet <quentin.thuet@gmail.com>
2025-08-27 21:13:28 +0200

    chore: change inherited scenes into classes

--- Scenes/trigger_zone.gd
+++
@@ -1,4 +0,0 @@
-extends Node
-
-@export var height = 0 
-@export var shout_name = ""
--- Scripts/obstacle.gd
+++ Scripts/obstacle.gd
@@ -1,4 +1,6 @@
-extends Node
+extends Area2D
+
+class_name Obstacle
 
 @export var height = 0 
 @export var shout_name = ""
